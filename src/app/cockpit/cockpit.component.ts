1f5744a257f9f1349826a18341d9c0db0d9f530f
Dhiru <dhirajdunani2001@gmail.com>
2021-12-25 20:50:41 +0530

    local reference pratice done!

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,24 +6,29 @@ import { Component, Input, OnInit, Output , EventEmitter } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  // output declarator stating that it need to send something out of the component
   @Output()
+  // we are creating the EventEmitter object and so we can emit the event to the parent tag 
   serverCreated =new EventEmitter<{name:string , content:string}>() ; 
-  @Output()
+
+  // we can also give the alias to the decorator  
+  @Output('bprintCreated')
   blueprintCreated = new EventEmitter<{name:string , content:string}>() ; 
   constructor() { }
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
-  onAddServer() {
+  onAddServer(serverName:HTMLInputElement) {
+    // we are emitting the response to the parent..
     this.serverCreated.emit({
-      name:this.newServerName , 
+      name : serverName.value , 
       content : this.newServerContent
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverName : HTMLInputElement) {
     this.blueprintCreated.emit({
-      name:this.newServerName , 
+      name:serverName.value , 
       content : this.newServerContent
     })
   }
