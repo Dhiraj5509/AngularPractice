9314db9f867ba111f94c283070e578c19c4e6051
Dhiru <dhirajdunani2001@gmail.com>
2021-12-26 20:22:39 +0530

    input with hostbinding

--- src/app/directives/better.directive.ts
+++ src/app/directives/better.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetter]'
 })
 export class BetterDirective implements OnInit {
 
-  constructor(private elementRef:ElementRef , private renderer : Renderer2) {
+  @Input()
+  bgColor : string = 'transparent' ; 
 
+  @HostBinding('style.width.px') width : number = 50 ; 
+  @HostBinding('style.width.px') height : number = 50 ; 
+  @HostBinding('style.backgroundColor') backGroundColor : string = 'violet' ; 
+
+  constructor(private elementRef:ElementRef , private renderer : Renderer2) {
   }
 
   ngOnInit(): void {
@@ -16,14 +22,19 @@ export class BetterDirective implements OnInit {
   // we can  listen to the event on our own directives 
   @HostListener('mouseover') onHover()
   {
-    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'black') ; 
-    this.renderer.setStyle(this.elementRef.nativeElement , 'color' , 'white') ; 
+    // this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'black') ; 
+    // this.renderer.setStyle(this.elementRef.nativeElement , 'color' , 'white') ; 
+
+    this.backGroundColor = this.bgColor ; 
   }
 
   @HostListener('mouseout') onOut()
   {
-    this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'blue') ; 
-    this.renderer.setStyle(this.elementRef.nativeElement , 'color' , 'white') ; 
+    // this.renderer.setStyle(this.elementRef.nativeElement , 'background-color' , 'blue') ; 
+    // this.renderer.setStyle(this.elementRef.nativeElement , 'color' , 'white') ; 
+    this.backGroundColor = 'violet' ; 
+    this.width = 100 ; 
+    this.height = 100 ; 
   }
 
 }
